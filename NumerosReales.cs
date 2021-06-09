5c03421c441f709424518f76ebde1c00ddbfbbcd
Antonio Enríquez <antonio.enralv@gmail.com>
2021-06-09 02:10:39 -0500

    estructura del programa

--- NumerosReales.cs
+++ NumerosReales.cs
@@ -5,6 +5,16 @@ class NumerosReales:Calcular{
 
     public override float suma()
     {
+        System.Console.WriteLine("Por favor ingresa el primer número a sumar: ");
+        var ingreso = Console.ReadLine();
+        float primerNumero = float.Parse(ingreso);
+        System.Console.WriteLine("Por favor ingresa el segundo número a sumar: ");
+        var ingreso2 = Console.ReadLine();
+        float segundoNumero = float.Parse(ingreso2);
+        System.Console.WriteLine("El primer número ingresado fue {0}", primerNumero);
+        System.Console.WriteLine("El segundo número ingresado fue {0}", segundoNumero);
+        float total = primerNumero + segundoNumero;
+        System.Console.WriteLine("El resultado de la suma es {0}", total);
         return base.concepto;
     }
     public override float resta()
--- Program.cs
+++ Program.cs
@@ -6,14 +6,241 @@ namespace DPRN2_U3_AC_ANEA
     {
         static void Main(string[] args)
         {
+            
+            string suma = "1.- Suma";
+            string resta = "2.- Resta";
+            string multiplicacion = "3.- Multiplicación";
+            string division = "4.- División";
+            string alCuadrado = "5.- Al Cuadrado";
+            string potencia="6.- Potencia";
+            string elegirMetodo ="Por favor elige el método que deseas calcular:\n";
 
-            try
+            System.Console.WriteLine("Bienvenido. Por favor ingresa el concepto a calcular.");  
+            System.Console.WriteLine("1.- Números Reales\n2.- Fracciones\n3.- Matrices");
+            var opcion = Console.ReadLine();
+            int elegirConcepto = int.Parse(opcion);
+            switch(elegirConcepto)
             {
+                case 1 when (elegirConcepto==1):
+                //Se abre un nuevo menú para elegir el método
+                System.Console.WriteLine(elegirMetodo);
+                System.Console.WriteLine(suma+resta+multiplicacion+division+alCuadrado+potencia);
+                var opcionMetodo = Console.ReadLine();
+                int metodoElegido = int.Parse(opcionMetodo);
+                if(metodoElegido==1)
+                {
+                    try
+                    {
+                        NumerosReales numerosreales = new NumerosReales(1,"Suma");
+                        numerosreales.suma();
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido==2)
+                {
+                    try
+                    {
 
-            }
-            catch(Exception ex)
-            {
-                System.Console.WriteLine(ex);
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido==3)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido==4)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido==5)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if(metodoElegido==6)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                break;
+                case 2 when (elegirConcepto==2):
+                //Se abre un nuevo menú para elegir el método
+                System.Console.WriteLine(elegirMetodo);
+                System.Console.WriteLine(suma+resta+multiplicacion+division+alCuadrado+potencia);
+                var opcionMetodo2 = Console.ReadLine();
+                int metodoElegido2 = int.Parse(opcionMetodo2);
+                if(metodoElegido2==1)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido2==2)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido2==3)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido2==4)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido2==5)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if(metodoElegido2==6)
+                {
+                                        try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                break;
+                case 3 when (elegirConcepto==3):
+                //Se abre un nuevo menú para elegir el método
+                System.Console.WriteLine(elegirMetodo);
+                System.Console.WriteLine(suma+resta+multiplicacion+division+alCuadrado+potencia);
+                var opcionMetodo3 = Console.ReadLine();
+                int metodoElegido3 = int.Parse(opcionMetodo3);
+                if(metodoElegido3==1)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido3==2)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido3==3)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido3==4)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if (metodoElegido3==5)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                else if(metodoElegido3==6)
+                {
+                    try
+                    {
+
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+                break;
             }
         }
     }
