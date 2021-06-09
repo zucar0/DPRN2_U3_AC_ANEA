f4202e026d9e0c2e8c775ce6ea194dc4878d28ab
Antonio Enríquez <antonio.enralv@gmail.com>
2021-06-09 13:32:01 -0500

    Fracciones OK

--- Fracciones.cs
+++ Fracciones.cs
@@ -5,26 +5,103 @@ class Fracciones:Calcular{
 
     public override float suma()
     {
+        double dividendo1, dividendo2, divisor1, divisor2, resultado;
+        Console.Write("Ingresa el valor del primer dividendo: ");
+        dividendo1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del primer divisor: ");
+        divisor1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo dividendo: ");
+        dividendo2 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo divisor: ");
+        divisor2 = double.Parse(Console.ReadLine());
+        resultado=(divisor1*dividendo2+divisor2*dividendo1)/dividendo2/dividendo1;
+        Console.WriteLine("El resultado es: " + resultado);
         return base.concepto;
     }
     public override float resta()
     {
+        double dividendo1, dividendo2, divisor1, divisor2, resultado;
+        Console.Write("Ingresa el valor del primer dividendo: ");
+        dividendo1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del primer divisor: ");
+        divisor1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo dividendo: ");
+        dividendo2 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo divisor: ");
+        divisor2 = double.Parse(Console.ReadLine());
+        resultado=(divisor1*dividendo2-divisor2*dividendo1)/dividendo2/dividendo1;
+        Console.WriteLine("El resultado de restar: {0}/{1}-{2}/{3} es {4}", dividendo1,divisor1, dividendo2,divisor2, resultado);
+
         return base.concepto;
     }
     public override float multiplicacion()
-    {
+    {   
+        double numerador1, denominador1, numerador2, denominador2, resultado;
+        Console.Write("Ingresa el valor del primer numerador: ");
+        numerador1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del primer denominador: ");
+        denominador1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo numerador: ");
+        numerador2 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo denominador: ");
+        denominador2 = double.Parse(Console.ReadLine());
+        double resultadoNumerador = 0;
+        double resultadoDenominador=0;
+        //Calculo del numerador del resultado
+        resultadoNumerador= numerador1*numerador2;
+        //Cálculo del denominador del resultado
+        resultadoDenominador=denominador1*denominador2;
+        resultado=numerador1/denominador1*numerador2/denominador2;
+        double comprobacionDeResultado=resultadoNumerador/resultadoDenominador;
+        System.Console.WriteLine("El resultado es {0}", resultado);
+        System.Console.WriteLine("La comprobación es {0}",comprobacionDeResultado);
         return base.concepto;
     }
     public override float division()
     {
+        double dividendo1, dividendo2, divisor1, divisor2, resultado;
+        Console.Write("Ingresa el valor del primer dividendo: ");
+        dividendo1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del primer divisor: ");
+        divisor1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo dividendo: ");
+        dividendo2 = double.Parse(Console.ReadLine());
+        Console.Write("Ingresa el valor del segundo divisor: ");
+        divisor2 = double.Parse(Console.ReadLine());
+        double resultadoNumerador = 0;
+        double resultadoDenominador=0;
+        resultadoNumerador=dividendo1*divisor2;
+        resultadoDenominador=dividendo2*divisor1;
+        resultado=(dividendo1/divisor1)/(dividendo2/divisor2);
+        double comprobacionDeResultado=resultadoNumerador/resultadoDenominador;
+        System.Console.WriteLine("El resultado es {0}", resultado);
+        System.Console.WriteLine("La comprobación es {0}",comprobacionDeResultado);
         return base.concepto;
     }
     public override double alCuadrado()
     {
+        System.Console.WriteLine("Por favor ingresa la fracción a elevar al cuadrado. \nDivideno: ");
+        double dividendo = double.Parse(Console.ReadLine());
+        System.Console.WriteLine("Divisor: ");
+        double divisor = double.Parse(Console.ReadLine());
+        double resultadoFraccion = dividendo/divisor;
+        System.Console.WriteLine("El número ingresado fue {0}/{1}", dividendo, divisor);
+        double total = Math.Pow(resultadoFraccion, 2);
+        System.Console.WriteLine("El resultado de elevar {0}/{1} al cudrado es {2}", dividendo, divisor, total);
         return base.concepto;
     }
     public override double potencia()
     {
+        System.Console.WriteLine("Por favor ingresa la fracción a potenciar. \nDivideno: ");
+        double dividendo = double.Parse(Console.ReadLine());
+        System.Console.WriteLine("Divisor: ");
+        double divisor = double.Parse(Console.ReadLine());
+        double resultadoFraccion = dividendo/divisor;
+        System.Console.WriteLine("Ahora ingresa la potencia a la que quieres elevar la fracción: ");
+        double potencia = double.Parse(Console.ReadLine());
+        System.Console.WriteLine("El número ingresado fue {0}/{1}", dividendo, divisor);
+        double total = Math.Pow(resultadoFraccion, potencia);
+        System.Console.WriteLine("El resultado de elevar {0}/{1} a la potencia {2} es {3}", dividendo, divisor, potencia,total);
         return base.concepto;
     }
 }
--- Matrices.cs
+++ Matrices.cs
@@ -1,6 +1,8 @@
 using System;
 class Matrices:Calcular{
 
+    public string impimir = "¿Quiéres imprimir la matriz";
+    public string opcionImprimir = "1.- Sí. 2.- No.";
     public Matrices(int conceptoSeleccionado, string metodoACalcular):base(conceptoSeleccionado, metodoACalcular){ }
 
     public override float suma()
@@ -41,9 +43,61 @@ class Matrices:Calcular{
         System.Console.WriteLine("Indícamos el número de columnas: ");
         var opcionColumnas = Console.ReadLine();
         int numeroDeColumnas = int.Parse(opcionFilas);
+        //Casificar la matriz dependiendo el tipo de matriz creada. 
+        //(Matriz triangular superior, Matriz triangular inferior, Matriz diagonal ó Matriz unidad).
+
+        //Imprimir la matriz
+        System.Console.WriteLine(impimir);
+        System.Console.WriteLine(opcionImprimir);
+        var respuestaImprimir = Console.ReadLine();
+        double imprimirSelect = double.Parse(respuestaImprimir);
+        if(imprimirSelect==1)
+        {
+            System.Console.WriteLine("Impresión");
+        }
+        else if (imprimirSelect==2)
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            System.Console.WriteLine("Haz elegido una opción inválidad. Se cerrará el programa.");
+            Environment.Exit(0);
+        }
     }
     public void matrizCuadradaAutomatica()
     {
-        
+        //b) Generar una matriz cuadrada automáticamente y de forma aleatoria
+        System.Console.WriteLine("Vamos a generar una matriz cuadrada de forma aleatoria.");
+        //Generarla de prueba de 5x5.
+
+        //el usuario elegirá el tipo de matriz cuadrada a crear. 
+        //(Matriz triangular superior, Matriz triangular inferior, Matriz diagonal ó Matriz unidad)
+        System.Console.WriteLine("Elige la matriz cuadrada a crear");
+        System.Console.WriteLine("1.- Matriz triangular superior");
+        System.Console.WriteLine("2.- Matriz triangular inferior");
+        System.Console.WriteLine("3.- Matriz diagonal");
+        System.Console.WriteLine("4.- Matriz unidad");
+        var respuestaMatriz = Console.ReadLine();
+        double matrizElegida = double.Parse(respuestaMatriz);
+
+        //Imprimir la matriz
+        System.Console.WriteLine(impimir);
+        System.Console.WriteLine(opcionImprimir);
+        var respuestaImprimir = Console.ReadLine();
+        double imprimirSelect = double.Parse(respuestaImprimir);
+        if(imprimirSelect==1)
+        {
+            System.Console.WriteLine("Impresión");
+        }
+        else if (imprimirSelect==2)
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            System.Console.WriteLine("Haz elegido una opción inválidad. Se cerrará el programa.");
+            Environment.Exit(0);
+        }
     }
 }
